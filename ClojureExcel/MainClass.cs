14c790389f0c5eb03e26f40cea55be92af2d8643
Matthew Molloy <whamtet@gmail.com>
2015-01-31 22:30:03 +0800

    Good riddance VBA

---
+++ ClojureExcel/MainClass.cs
@@ -0,0 +1,281 @@
+﻿using clojure.lang;
+using ExcelDna.Integration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace ClojureExcel
+{
+    public static class MainClass
+    {
+        private static IFn read_string = clojure.clr.api.Clojure.var("clojure.core", "read-string");
+        private static IFn eval = clojure.clr.api.Clojure.var("clojure.core", "eval");
+
+        private static Object doublize(object o)
+        {
+            if (o is Ratio)
+            {
+                return ((Ratio)o).ToDouble(null);
+            }
+            if (o == null)
+            {
+                return "nil";
+            }
+            else
+            {
+                return o;
+            }
+        }
+
+        private static object seqize(Object o)
+        {
+            if (o is IPersistentCollection) {
+                o = ((IPersistentCollection)o).seq();
+            }
+            if (o is ISeq)
+            {
+                ISeq r2 = (ISeq)o;
+                object[] outArr = new object[r2.count()];
+                int i = 0;
+                while (r2 != null)
+                {
+                    outArr[i] = doublize(r2.first());
+                    i++;
+                    r2 = r2.next();
+                }
+                return outArr;
+            }
+            else
+            {
+                return doublize(o);
+            }
+        }
+        
+        private static Object my_eval(String input)
+        {
+            Object o;
+            try
+            {
+                o = eval.invoke(read_string.invoke(input));
+            }
+            catch (Exception e)
+            {
+                return pack(Regex.Split(e.ToString(), "\n"));
+            }
+
+            if (o is IPersistentCollection) {
+                o = ((IPersistentCollection)o).seq();
+            }
+            if (o is ISeq)
+            {
+                ISeq r2 = (ISeq)o;
+                object[] outArr = new object[r2.count()];
+                int i = 0;
+                while (r2 != null) {
+                    outArr[i] = seqize(r2.first());
+                    i++;
+                    r2 = r2.next();
+                }
+                return pack(outArr);
+            }
+            else
+            {
+                return pack(doublize(o));
+            }
+        }
+
+        private static String stringify(object o)
+        {
+            String s = o.GetType().Name;
+            switch (s)
+            {
+                case "String":
+                    return "\"" + o + "\"";
+                case "ExcelEmpty":
+                    return "nil";
+                case "ExcelMissing":
+                    return "nil";
+                case "ExcelError":
+                    return "nil";
+                default:
+                    return o.ToString();
+            }
+        }
+        private static String stringifies(object[] o)
+        {
+            if (o.Length == 1)
+            {
+                return stringify(o[0]) + " ";
+            }
+            else
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("[");
+                foreach (object o2 in o)
+                {
+                    sb.Append(stringify(o2) + " ");
+                }
+                sb.Append("] ");
+                return sb.ToString();
+            }
+        }
+        private static String stringifies2(params object[][] o)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (object[] o2 in o)
+            {
+                sb.Append(stringifies(o2));
+            }
+            return sb.ToString();
+        }
+
+        [ExcelFunction(Description = "Get Version")]
+        public static String GetVersion()
+        {
+            return "0.0.1";
+        }
+        private static object pack(object o) {
+            if (o is object[])
+            {
+                object[] o2 = (object[])o;
+                int m = o2.Length;
+                m = m < 2 ? 2 : m;
+                int n = 2;
+                foreach (object o3 in o2)
+                {
+                    if (o3 is object[])
+                    {
+                        object[] o4 = (object[])o3;
+                        if (o4.Length > n)
+                        {
+                            n = o4.Length;
+                        }
+                    }
+                }
+                var oot = new object[m, n];
+                for (int i = 0; i < m; i++)
+                {
+                    if (i >= o2.Length)
+                    {
+                        for (int j = 0; j < n; j++)
+                        {
+                            oot[i, j] = "nil";
+                        }
+                    }
+                    else
+                    {
+                        object o3 = o2[i];
+                        if (o3 is object[])
+                        {
+                            object[] o4 = (object[])o3;
+                            for (int j = 0; j < o4.Length; j++)
+                            {
+                                oot[i, j] = o4[j];
+                            }
+                            for (int j = o4.Length; j < n; j++)
+                            {
+                                oot[i, j] = "nil";
+                            }
+                        }
+                        else
+                        {
+                            oot[i, 0] = o3;
+                            for (int j = 1; j < n; j++)
+                            {
+                                oot[i, j] = "nil";
+                            }
+                        }
+                    }
+                }
+                return oot;
+            }
+            else
+            {
+                var oot = new object[2, 2];
+                oot[0, 0] = o;
+                oot[0, 1] = "nil";
+                oot[1, 0] = "nil";
+                oot[1, 1] = "nil";
+                return oot;
+            }
+        }
+        
+
+        [ExcelFunction(Description = "My first .NET function")]
+        public static object Load(Object[] name)
+        {
+            
+            StringBuilder input = new StringBuilder();
+            input.Append("(do \n");
+            foreach (Object s in name)
+            {
+                if (s.GetType() != typeof(ExcelEmpty))
+                {
+                    input.Append(s + "\n");
+                }
+            }
+            input.Append(")");
+            
+            return my_eval(input.ToString());
+            
+        }
+        [ExcelFunction(Description = "Evaluate in Octant")]
+        public static Object Invoke1(String f, Object[] a0)
+        {
+            String s = "(" + f + " " + stringifies2(a0) + ")";
+            return my_eval(s);
+        }
+        [ExcelFunction(Description = "Evaluate in Octant")]
+        public static Object Invoke2(String f, Object[] a0, Object[] a1)
+        {
+            String s = "(" + f + " " + stringifies2(a0, a1) + ")";
+            return my_eval(s);
+        }
+        [ExcelFunction(Description = "Evaluate in Octant")]
+        public static Object Invoke3(String f, Object[] a0, Object[] a1, Object[] a2)
+        {
+            String s = "(" + f + " " + stringifies2(a0, a1, a2) + ")";
+            return my_eval(s);
+        }
+        [ExcelFunction(Description = "Evaluate in Octant")]
+        public static Object Invoke4(String f, Object[] a0, Object[] a1, Object[] a2, Object[] a3)
+        {
+            String s = "(" + f + " " + stringifies2(a0, a1, a2, a3) + ")";
+            return my_eval(s);
+        }
+        [ExcelFunction(Description = "Evaluate in Octant")]
+        public static Object Invoke5(String f, Object[] a0, Object[] a1, Object[] a2, Object[] a3, Object[] a4)
+        {
+            String s = "(" + f + " " + stringifies2(a0, a1, a2, a3, a4) + ")";
+            return my_eval(s);
+        }
+        [ExcelFunction(Description = "Evaluate in Octant")]
+        public static Object Invoke6(String f, Object[] a0, Object[] a1, Object[] a2, Object[] a3, Object[] a4, Object[] a5)
+        {
+            String s = "(" + f + " " + stringifies2(a0, a1, a2, a3, a4, a5) + ")";
+            return my_eval(s);
+        }
+        [ExcelFunction(Description = "Evaluate in Octant")]
+        public static Object Invoke7(String f, Object[] a0, Object[] a1, Object[] a2, Object[] a3, Object[] a4, Object[] a5, Object[] a6)
+        {
+            String s = "(" + f + " " + stringifies2(a0, a1, a2, a3, a4, a5, a6) + ")";
+            return my_eval(s);
+        }
+        [ExcelFunction(Description = "Evaluate in Octant")]
+        public static Object Invoke8(String f, Object[] a0, Object[] a1, Object[] a2, Object[] a3, Object[] a4, Object[] a5, Object[] a6, Object[] a7)
+        {
+            String s = "(" + f + " " + stringifies2(a0, a1, a2, a3, a4, a5, a6, a7) + ")";
+            return my_eval(s);
+        }
+        [ExcelFunction(Description = "Evaluate in Octant")]
+        public static Object Invoke9(String f, Object[] a0, Object[] a1, Object[] a2, Object[] a3, Object[] a4, Object[] a5, Object[] a6, Object[] a7, Object[] a8)
+        {
+            String s = "(" + f + " " + stringifies2(a0, a1, a2, a3, a4, a5, a6, a7, a8) + ")";
+            return my_eval(s);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,60 @@
+#Excel REPL
+
+As much as we all love VBA there are better languages available such as ... Clojure.  Excel REPL makes it easy to start a ClojureCLR Repl from within Excel.  Simply install it as an Excel Add-In to provide two additional Excel Functions
+
+##Load
+
+    =Load(A:A)
+
+Concatenates the contents of selected cells and evaluates them within a do loop.
+
+##Invoke
+
+    =Invoke1("myFunction", A1)
+    =Invoke2("myFunction", A2:A5, C6)
+    ...
+
+Invokes myFunction with the supplied arguments and returns the result.  Range arguments are provided as a Clojure vector unless the range is of length one, in which case they are the same as a single cell argument.
+
+Arity cannot be overloaded so you must use Invoke1, Invoke2 etc.  For functions with zero arguments simply use Load.
+
+##Returning 1D and 2D arrays
+
+If your function returns a 1 or 2 dimensional collection you may paste it into a range of Excel Cells.  To do so
+
+1) Drag from the top left hand corner the number of cells for your output
+
+2) Click in the formula bar and enter your formula
+
+3) Press Control + Shift + Enter instead of simply enter
+
+##Error Messages
+
+Errors are caught and returned as text within the output cells.  The stacktrace is split down the column so select multiple cells for output as mentioned above.
+
+##Auxilliary Methods
+
+ClojureCLR does not implement slurp for http by default.  Use the Auxilliary class as follows
+
+(import ClojureExcel.AuxClass)
+(AuxClass/slurpSite "http://www.google.com")
+
+##Build and Installation
+
+Excel Repl is a Microsoft Visual Studio solution.  Install Visual Studio 2013 (the free Express Edition is sufficient) and import this repository.  Visual Studio's package manager is called NuGet.  The NuGet dependencies are Clojure and Excel-DNA.
+
+To Install into Excel without Visual Studio copy the contents of ClojureExcel/bin/Debug into %HomeDirectory%\AppData\Roaming\Microsoft\AddIns.  Then open Excel
+
+1) For Excel 2013 select File > Options > Add-Ins > Go (at the bottom) > Browse.  Select ClojureExcel-AddIn.xll and click ok.
+
+2) For earlier editions click Tools > Add-Ins > Browse and select ClojureExcel-AddIn.xll.
+
+Provided that you have copied the binary files correctly Excel Repl will automatically load every time you start Excel.
+
+##System Requirements
+
+Excel Repl works with Microsoft Excel 97+ (that's quite old) and Microsoft .NET 4.0 or 4.5.
+
+##Good Riddance VBA
+
+What a crap language.  Lisp existed then, so why did Microsoft create VBA?  Because they're Microsoft.
