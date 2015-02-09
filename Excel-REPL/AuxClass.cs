75ac68788f7413be3d8fdf0198f81b07670fd567
Matthew Molloy <whamtet@gmail.com>
2015-02-09 15:55:58 +0800

    adding new files location

---
+++ Excel-REPL/AuxClass.cs
@@ -0,0 +1,39 @@
+﻿using clojure.lang;
+using ExcelDna.Integration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+
+namespace ClojureExcel
+{
+    public static class AuxClass
+    {
+        public static string slurpSite(String uri)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            Stream receiveStream = response.GetResponseStream();
+            StreamReader readStream = null;
+
+            if (response.CharacterSet == null)
+            {
+                readStream = new StreamReader(receiveStream);
+            }
+            else
+            {
+                readStream = new StreamReader(receiveStream, Encoding.GetEncoding(response.CharacterSet));
+            }
+
+            string data = readStream.ReadToEnd();
+
+            response.Close();
+            readStream.Close();
+            return data;
+        }
+    }
+}
